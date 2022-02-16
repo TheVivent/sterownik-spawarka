38684ad24a97ce59f0467919de4620bc27dfea77
Adam Kapuściński <ziemniaczek320@gmail.com>
2022-02-16 19:37:59 +0100

    i2c nie działa
    nwm czemu :(

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,11 @@
-#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(128, 64, &Wire, OLED_RESET);
 
 // general
 int wait_time = 10;
@@ -32,10 +39,6 @@ int detect_rotation_direction()
 
 void weld()
 {
-  // if (do_weld) {
-  //   digitalWrite(RELAY, HIGH);
-
-  // }
   digitalWrite(RELAY, HIGH);
   delay(wait_time);
   digitalWrite(RELAY, LOW);
@@ -43,13 +46,43 @@ void weld()
 
 void setup()
 {
+  Serial.begin(9600);
+  while (!Serial)
+  {
+    ;
+  }
+  delay(100);
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  { // Address 0x3D for 128x64
+    Serial.println("SSD1306 allocation failed");
+    for (;;)
+      ; // Don't proceed, loop forever
+  }
+  delay(2000);
+
+  Serial.println("Writing to display");
+  display.clearDisplay();
+  display.setTextSize(3);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 20);
+  display.println("Hello, world!");
+  display.display();
+  delay(2000);
+  Serial.println("Done writing to display");
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  // display.display();
+  // delay(2000);
+
   pinMode(ROT_CLK, INPUT);
   pinMode(ROT_DT, INPUT);
   pinMode(ROT_SW, INPUT_PULLUP);
   pinMode(RELAY, OUTPUT);
   digitalWrite(RELAY, LOW);
   rotary_encoder_pos = digitalRead(ROT_CLK);
-  Serial.begin(9600);
 }
 
 void loop()
@@ -66,12 +99,13 @@ void loop()
   }
   last_wait_time = wait_time;
 
+  // Clear the buffer
+  // display.clearDisplay();
+  // display.drawRect(10, 10, 10, 10, WHITE);
+  // display.display();
+
   // ----------------- ODCZYT PRZYCISKU -----------------
-  // potestować z mniejszym czasem
-  // stabilizacja napięcia trwa za długo
-  // i nieraz jeżeli czas jest za krótki,
-  // to potrafi uruchomić się przy odpuszczeniu
-  delay(10);
+  delay(2);
   if (digitalRead(ROT_SW) == LOW)
   {
     if (!was_pressed)
